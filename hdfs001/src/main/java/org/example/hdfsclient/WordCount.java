a52197e5488bd621445ad3550681c33210868e76
c1rew <cholyonzh@gmail.com>
2020-06-03 23:58:13 +0800

    * modify wordcount

--- hdfs001/src/main/java/org/example/hdfsclient/WordCount.java
+++ hdfs001/src/main/java/org/example/hdfsclient/WordCount.java
@@ -1,9 +1,13 @@
 package org.example.hdfsclient;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.GenericOptionsParser;
@@ -41,9 +45,26 @@ public class WordCount {
         }
     }
 
-    public static void main(String [] args) throws IOException {
+    public static void main(String [] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 2){
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
 
+        Job job = new Job(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        for (int i =0; i<otherArgs.length-1;++i) {
+            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length -1]));
+
+        System.exit(job.waitForCompletion(true) ? 0:1);
     }
 }
