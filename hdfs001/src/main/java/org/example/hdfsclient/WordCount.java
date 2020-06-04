6c57555f1aaabb02fbd737a04b908943107bac19
c1rew <cholyonzh@gmail.com>
2020-06-04 23:53:06 +0800

    *modify word count implementation

--- hdfs001/src/main/java/org/example/hdfsclient/WordCount.java
+++ hdfs001/src/main/java/org/example/hdfsclient/WordCount.java
@@ -5,11 +5,11 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
@@ -18,15 +18,25 @@ import java.util.StringTokenizer;
 public class WordCount {
     public static class TokenizerMapper extends
             Mapper<LongWritable, Text, Text, IntWritable> {
+        private Text word = new Text();
+        private IntWritable one = new IntWritable(1);
 
-        public void map(LongWritable longWritable, Text value, Context context) throws IOException, InterruptedException {
-            Text keyOut;
-            IntWritable valueOut = new IntWritable(1);
+        @Override
+        public void map(LongWritable longWritable, Text value, Context context)
+                throws IOException, InterruptedException {
 
-            StringTokenizer token = new StringTokenizer(valueOut.toString());
-            while (token.hasMoreTokens()) {
-                keyOut = new Text(token.nextToken());
-                context.write(keyOut, valueOut);
+//            Text keyOut;
+//            StringTokenizer token = new StringTokenizer(value.toString());
+//            while (token.hasMoreTokens()) {
+//                keyOut = new Text(token.nextToken());
+//                context.write(keyOut, one);
+//            }
+            String line = value.toString();
+            String[] words = line.split(" ");
+
+            for (String word : words) {
+                this.word.set(word);
+                context.write(this.word, this.one);
             }
         }
     }
@@ -35,6 +45,7 @@ public class WordCount {
             Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
+        @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
@@ -45,26 +56,36 @@ public class WordCount {
         }
     }
 
-    public static void main(String [] args) throws IOException, ClassNotFoundException, InterruptedException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        if (otherArgs.length < 2){
-            System.err.println("Usage: wordcount <in> <out>");
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: wordcount <in> [<in>...] <out>");
             System.exit(2);
         }
-
-        Job job = new Job(conf, "word count");
+        // 1. 获取一个job实例
+        Job job = Job.getInstance(conf, "word count");
+        // 2. 设置类路径
         job.setJarByClass(WordCount.class);
+        // 3. 设置Mapper和Reducer
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
+        //job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
+
+        // 4. 设置Mapper和Reducer输出类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        for (int i =0; i<otherArgs.length-1;++i) {
+
+        // 5. 设置输入输出参数
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
             FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
         }
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length -1]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
 
-        System.exit(job.waitForCompletion(true) ? 0:1);
+        // 6. 提交job
+        boolean b = job.waitForCompletion(true);
+        System.exit(b ? 0 : 1);
     }
 }
