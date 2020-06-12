159d55946ce244c3e4668310c1fb978239133540
c1rew <cholyonzh@gmail.com>
2020-06-12 13:12:23 +0800

    *modify sortdriver

--- hadoop/hdfs001/src/main/java/org/example/writablecomparable/SortDriver.java
+++ hadoop/hdfs001/src/main/java/org/example/writablecomparable/SortDriver.java
@@ -1,7 +1,37 @@
 package org.example.writablecomparable;
 
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 /**
  * 针对flow的结果，按照总流量进行排序
  */
 public class SortDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Job job = Job.getInstance(new Configuration());
+
+        job.setJarByClass(SortDriver.class);
+
+        job.setMapperClass(SortMapper.class);
+        job.setReducerClass(SortReducer.class);
+
+        job.setMapOutputKeyClass(FlowBean.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean b = job.waitForCompletion(true);
+        System.exit(b ? 0 : 1);
+    }
 }
