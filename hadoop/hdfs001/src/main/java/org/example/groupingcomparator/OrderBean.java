132a833798398354afb7eb33ccc359dd8962a172
c1rew <cholyonzh@gmail.com>
2020-06-15 23:58:01 +0800

    +add grouping comparator example

---
+++ hadoop/hdfs001/src/main/java/org/example/groupingcomparator/OrderBean.java
@@ -0,0 +1,66 @@
+package org.example.groupingcomparator;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * @author c1rew
+ * @create 2020-06-15 23:52
+ */
+public class OrderBean implements WritableComparable<OrderBean> {
+
+    private String orderId;
+    private String productId;
+    private double price;
+
+    @Override
+    public String toString() {
+        return orderId + '\t' + productId + '\t' + price;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int compareTo(OrderBean o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeUTF(orderId);
+        dataOutput.writeUTF(productId);
+        dataOutput.writeDouble(price);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.orderId = dataInput.readUTF();
+        this.productId = dataInput.readUTF();
+        this.price = dataInput.readDouble();
+    }
+}
