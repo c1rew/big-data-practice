de404c5078c3973c8241796c1ec5ac9c46b79146
c1rew <cholyonzh@gmail.com>
2020-06-22 23:06:10 +0800

    +add thread anonymous subclass

--- Java/JavaSenior/src/main/java/org/example/exer/ThreadDemo.java
+++ Java/JavaSenior/src/main/java/org/example/exer/ThreadDemo.java
@@ -13,6 +13,29 @@ public class ThreadDemo {
 
         thread1.start();
         thread2.start();
+
+        // 创建Thread类的匿名子类
+        new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(Thread.currentThread().getName() + " " + i);
+                    }
+                }
+            }
+        }.start();
+
+        new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    if (i % 2 != 0) {
+                        System.out.println(Thread.currentThread().getName() + " " + i);
+                    }
+                }
+            }
+        }.start();
     }
 }
 
