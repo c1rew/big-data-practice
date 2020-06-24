a0b16bd7c28fb8656b0854159f64f1e4b351d747
c1rew <cholyonzh@gmail.com>
2020-06-24 16:38:14 +0800

    +add hdfs demo file

---
+++ hadoop/hdfs001/src/main/java/org/example/hdfsclient/HDFSDeleteEmptyDirAndFiles.java
@@ -0,0 +1,11 @@
+package org.example.hdfsclient;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+
+public class HDFSDeleteEmptyDirAndFiles {
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        new FileSystem.get()
+    }
+}
