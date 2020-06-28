98a70a02b43c692ed6100c7235c8cba43c9f5a25
c1rew <cholyonzh@gmail.com>
2020-06-28 17:42:36 +0800

    *modify maven conf and modify hdfs demo

--- hadoop/hdfs001/src/main/java/org/example/hdfsclient/HDFSDeleteEmptyDirAndFiles.java
+++ hadoop/hdfs001/src/main/java/org/example/hdfsclient/HDFSDeleteEmptyDirAndFiles.java
@@ -3,9 +3,26 @@ package org.example.hdfsclient;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 
+import java.io.IOException;
+import java.net.URI;
+
 public class HDFSDeleteEmptyDirAndFiles {
+
+    private static FileSystem fs = null;
+
     public static void main(String[] args) {
-        Configuration conf = new Configuration();
-        new FileSystem.get()
+        initialFileSystem();
     }
+
+    private static void initialFileSystem() {
+        Configuration conf = new Configuration();
+        try {
+            fs = FileSystem.get(URI.create("hdfs://hadoop277ha/"), conf, "lyon");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
