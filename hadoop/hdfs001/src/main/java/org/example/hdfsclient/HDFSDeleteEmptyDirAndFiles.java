1a125c6f69e88932f3dc0650a7ac75c0d0f2fc2f
c1rew <cholyonzh@gmail.com>
2020-06-30 17:22:05 +0800

    *implementation delete empty dir and files

--- hadoop/hdfs001/src/main/java/org/example/hdfsclient/HDFSDeleteEmptyDirAndFiles.java
+++ hadoop/hdfs001/src/main/java/org/example/hdfsclient/HDFSDeleteEmptyDirAndFiles.java
@@ -1,10 +1,9 @@
 package org.example.hdfsclient;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.*;
 
 import java.io.IOException;
-import java.net.URI;
 
 public class HDFSDeleteEmptyDirAndFiles {
 
@@ -12,15 +11,106 @@ public class HDFSDeleteEmptyDirAndFiles {
 
     public static void main(String[] args) {
         initialFileSystem();
+
+        //createTestData();
+
+        delEmptyDirAndFiles(new Path("/a"));
+
+        printTestData(new Path("/a"));
+        //delTestData();
+    }
+
+    private static void printTestData(Path path) {
+        try {
+            FileStatus[] listStatus = fs.listStatus(path);
+            for (FileStatus status : listStatus) {
+                if (status.isDirectory()) {
+                    System.out.println(status.getPath());
+                    printTestData(status.getPath());
+                } else if (status.isFile()) {
+                    System.out.println(status.getPath());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void delEmptyDirAndFiles(Path path) {
+        try {
+            FileStatus[] listStatus = fs.listStatus(path);
+            if (listStatus.length == 0) {
+                fs.delete(path,true);
+                return;
+            }
+            RemoteIterator<LocatedFileStatus> locatedStatus = fs.listLocatedStatus(path);
+            while (locatedStatus.hasNext()) {
+                LocatedFileStatus fileStatus = locatedStatus.next();
+                Path currentPath = fileStatus.getPath();
+                Path parentPath = currentPath.getParent();
+
+                if (fileStatus.isDirectory()) {
+                    if (fs.listStatus(currentPath).length == 0) {
+                        fs.delete(currentPath,true);
+                    } else {
+                        delEmptyDirAndFiles(currentPath);
+                    }
+                } else if (fileStatus.isFile()){
+                    if (fileStatus.getLen() == 0) {
+                        fs.delete(currentPath, true);
+                    }
+                }
+                if (fs.listStatus(parentPath).length == 0) {
+                    fs.delete(parentPath, true);
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void delTestData() {
+
+        try {
+            fs.delete(new Path("/a"), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createTestData() {
+
+        String emptyFile = "D:\\emptyfile.txt";
+        String notEmptyFile = "D:\\notemptyfile.txt";
+        try {
+            fs.mkdirs(new Path("/a/b/c1/d"));
+
+            fs.mkdirs(new Path("/a/b/c2/e"));
+            fs.copyFromLocalFile(new Path(emptyFile), new Path("/a/b/c2/e"));
+            fs.copyFromLocalFile(new Path(notEmptyFile), new Path("/a/b/c2/e"));
+
+            fs.mkdirs(new Path("/a/b/c3/f"));
+            fs.copyFromLocalFile(new Path(emptyFile), new Path("/a/b/c3/f"));
+
+            fs.mkdirs(new Path("/a/b/c4/g"));
+            fs.copyFromLocalFile(new Path(notEmptyFile), new Path("/a/b/c4/g"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void initialFileSystem() {
         Configuration conf = new Configuration();
         try {
-            fs = FileSystem.get(URI.create("hdfs://hadoop277ha/"), conf, "lyon");
+            System.setProperty("HADOOP_USER_NAME", "lyon");
+            conf.addResource("config/core-site.xml");
+            conf.addResource("config/hdfs-site.xml");
+            //fs = FileSystem.get(URI.create("hdfs://hadoop277ha/"), conf, "lyon");
+            // HA的调试环境需要使用xml配置进行指定
+            fs = FileSystem.get(conf);
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
