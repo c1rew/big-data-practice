5da0b0191751c2c8c8a98690c19b4a134301f6ab
c1rew <cholyonzh@gmail.com>
2020-10-07 09:09:18 +0800

    *add spark sql transform test

---
+++ Spark/learn_spark/src/main/scala/com/viper/spark/sql/SparkSQL_Transform.scala
@@ -0,0 +1,47 @@
+package com.viper.spark.sql
+
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+/**
+ * @author c1rew
+ * @create 2020-10-07 00:03
+ */
+object SparkSQL_Transform {
+    def main(args: Array[String]): Unit = {
+        // 创建sparkconf对象
+        val conf = new SparkConf().setMaster("local[*]").setAppName("SparkSQL01_Demo")
+
+        // 获取session对象
+        val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+
+        // 导入隐式转换
+        import spark.implicits._
+
+        // 创建RDD
+        val rdd: RDD[(Int, String, Int)] = spark.sparkContext.makeRDD(List((1, "Tom", 22), (2, "Jack", 23), (3, "Candy", 19)))
+
+        // rdd -> DataFrame
+        val df: DataFrame = rdd.toDF("id", "name", "age")
+
+        // DataFrame -> DataSet
+        val ds: Dataset[User] = df.as[User]
+
+        // DataSet -> DataFrame
+        val df1: DataFrame = ds.toDF()
+
+        // DataFrame -> rdd
+        val rdd1: RDD[Row] = df1.rdd
+
+        rdd1.foreach(row=>{
+            // 通过索引获取数据
+            println(row.getInt(0)+"\t"+row.getString(1)+"\t"+row.getInt(2))
+        })
+
+        // 释放资源
+        spark.close()
+    }
+}
+
+case class User(id: Int, name: String, age: Int)
