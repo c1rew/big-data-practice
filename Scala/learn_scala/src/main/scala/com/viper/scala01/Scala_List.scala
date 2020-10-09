24afa015ca3e19837067bbfe6a58d51369f864f9
c1rew <cholyonzh@gmail.com>
2020-10-10 00:06:31 +0800

    +add scala list test

---
+++ Scala/learn_scala/src/main/scala/com/viper/scala01/Scala_List.scala
@@ -0,0 +1,25 @@
+package com.viper.scala01
+
+/**
+ * @author c1rew    
+ * @create 2020-10-09 23:52
+ */
+object Scala_List {
+    def main(args: Array[String]): Unit = {
+        /**
+         * Java中的集合：List， Set，Map（可变）
+         * List：有序，可重复
+         * LinkedList，ArrayList
+         * Set：无序，不可重复
+         * HashSet，TreeSet
+         * Map：无序，key不可重复，value可重复
+         * HashMap，HashTable，TreeMap
+         *
+         * 数组（不可变）
+         * int[] ints = new int[4]
+         */
+
+        // Scala中集合分为两大类，可变 & 不可变
+    }
+
+}
