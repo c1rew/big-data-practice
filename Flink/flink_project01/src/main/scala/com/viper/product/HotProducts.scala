9d3fbd78fbed7fbe5a8376ab9ca6cfed78342d5f
c1rew <cholyonzh@gmail.com>
2020-11-30 07:31:58 +0800

    *update flink project

--- Flink/flink_project01/src/main/scala/com/viper/product/HotProducts.scala
+++ Flink/flink_project01/src/main/scala/com/viper/product/HotProducts.scala
@@ -1,11 +1,15 @@
 package com.viper.product
 
 import com.viper.utils.Utils
+import org.apache.flink.api.common.functions.AggregateFunction
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
+import org.apache.flink.streaming.api.functions.{AssignerWithPeriodicWatermarks, KeyedProcessFunction}
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.WindowFunction
 import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
 
 /**
  * @author c1rew    
@@ -23,6 +27,11 @@ object HotProducts {
                                sessionId: String // 会话id
                            )
 
+    // 窗口统计结果
+    case class ProductViewCount(productId: Long,
+                                WindowEnd: Long,
+                                count: Long)
+
     def main(args: Array[String]): Unit = {
         // 1. 获取执行环境
         val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
@@ -31,18 +40,50 @@ object HotProducts {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
 
-        env.readTextFile("")
-            .map(Utils.string2UserBehavior(_))
-            .assignTimestampsAndWatermarks(new EventTimeExtractor)
-            .filter(_.behaviorType == "P")
-            .keyBy(_.productId)
-            .timeWindow(Time.hours(1),Time.minutes(5))
-            .aggregate()
+        env.readTextFile("") // 读取数据
+            .map(Utils.string2UserBehavior(_)) // 解析数据
+            .assignTimestampsAndWatermarks(new EventTimeExtractor) // 指定watermark
+            .filter(_.behaviorType == "P") //过滤用户行为数据
+            .keyBy(_.productId) // 按商品进行分组
+            .timeWindow(Time.hours(1), Time.minutes(5))
+            .aggregate(new CountProduct,new WindowResult) // 计算窗口数据
+            .keyBy(_.WindowEnd)  // 按照窗口进行分组
+            .process()
 
 
 
         // 启动程序
         env.execute("hotProducts")
+    }
+
+    class TopHotProduct(topN:Int) extends KeyedProcessFunction[Long,ProductViewCount,String] {
+        override def processElement(i: ProductViewCount, context: KeyedProcessFunction[Long, ProductViewCount, String]#Context, collector: Collector[String]): Unit = ???
+    }
+
+
+    class WindowResult extends WindowFunction[Long,ProductViewCount,Long,TimeWindow] {
+        override def apply(key: Long,
+                           window: TimeWindow,
+                           input: Iterable[Long],
+                           out: Collector[ProductViewCount]): Unit = {
+            out.collect(ProductViewCount(key,window.getEnd,input.iterator.next()))
+        }
+    }
+
+
+    /**
+     * IN, 输入数据类型
+     * ACC，辅助变量数据类型
+     * OUT，输出数据类型
+     */
+    class CountProduct extends AggregateFunction[UserBehavior, Long, Long] {
+        override def createAccumulator(): Long = 0L
+
+        override def add(in: UserBehavior, acc: Long): Long = acc + 1
+
+        override def getResult(acc: Long): Long = acc
+
+        override def merge(acc: Long, acc1: Long): Long = acc + acc1
     }
 
     /**
