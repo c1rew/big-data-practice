e97cd3101e808b9d9b61c868d5ef5f7d2a6e5f0d
c1rew <cholyonzh@gmail.com>
2020-12-05 06:29:50 +0800

    +add collection test case

---
+++ Java/JavaSenior/day05/src/main/java/com/viper/CollectionTest.java
@@ -0,0 +1,20 @@
+package com.viper;
+
+/**
+ *
+ * 集合框架
+ * 1. Collection接口：单列集合，用来存储一个一个对象
+ *      List接口：存储有序、可重复的数据
+ *          ArrayList、LinkedList、Vector
+ *
+ *      Set接口：无序、不可重复的数据
+ *          HashSet、LinkedHashSet、TreeSet
+ *
+ * 2. Map接口：双列集合，一对一对的数据
+ *          HashMap、LinkedHashMap、TreeMap、HashTable、Properties
+ *
+ * @author c1rew
+ * @create 2020-12-04 21:02
+ */
+public class CollectionTest {
+}
