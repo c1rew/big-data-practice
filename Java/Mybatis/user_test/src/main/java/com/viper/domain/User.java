20a0bac37fc5f4a82f192b5e43bc718e439a863d
c1rew <cholyonzh@gmail.com>
2021-02-21 06:41:57 +0800

    add lombok and mapper

--- Java/Mybatis/user_test/src/main/java/com/viper/domain/User.java
+++ Java/Mybatis/user_test/src/main/java/com/viper/domain/User.java
@@ -1,8 +1,16 @@
 package com.viper.domain;
 
+import lombok.Data;
+
 /**
  * @author c1rew
  * @create 2021-02-20 23:30
  */
+@Data
 public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
 }
--- Java/Mybatis/user_test/src/main/java/com/viper/mapper/UserMapper.java
+++ Java/Mybatis/user_test/src/main/java/com/viper/mapper/UserMapper.java
@@ -1,8 +1,17 @@
 package com.viper.mapper;
 
+import com.viper.domain.User;
+
 /**
  * @author c1rew
  * @create 2021-02-20 23:30
  */
 public interface UserMapper {
+
+    /**
+     * 根据id获取用户信息
+     * @param id
+     * @return
+     */
+    User findById(Long id);
 }
