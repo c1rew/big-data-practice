093ac44c468e9a2d4ef078502d3b63c8919ff899
c1rew <cholyonzh@gmail.com>
2021-02-20 23:31:44 +0800

    add user test class

---
+++ Java/Mybatis/user_test/src/main/java/com/viper/domain/User.java
@@ -0,0 +1,8 @@
+package com.viper.domain;
+
+/**
+ * @author c1rew
+ * @create 2021-02-20 23:30
+ */
+public class User {
+}
---
+++ Java/Mybatis/user_test/src/main/java/com/viper/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.viper.mapper;
+
+/**
+ * @author c1rew
+ * @create 2021-02-20 23:30
+ */
+public interface UserMapper {
+}
