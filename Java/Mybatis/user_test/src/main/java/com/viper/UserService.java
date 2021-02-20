5f2e81d2fbdef3a143f76b70595ac4d9ff0461c4
c1rew <cholyonzh@gmail.com>
2021-02-21 06:50:06 +0800

    add userservice class

---
+++ Java/Mybatis/user_test/src/main/java/com/viper/UserService.java
@@ -0,0 +1,14 @@
+package com.viper;
+
+import org.junit.Test;
+
+/**
+ * @author c1rew
+ * @create 2021-02-21 06:43
+ */
+public class UserService {
+    @Test
+    public void findById() {
+
+    }
+}
