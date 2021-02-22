8dd49fefdc9659d91d0aa4d644cb832fecad1856
c1rew <cholyonzh@gmail.com>
2021-02-22 18:22:22 +0800

    test find user by id

--- Java/Mybatis/user_test/src/main/java/com/viper/UserService.java
+++ Java/Mybatis/user_test/src/main/java/com/viper/UserService.java
@@ -1,6 +1,14 @@
 package com.viper;
 
+import com.viper.domain.User;
+import com.viper.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * @author c1rew
@@ -8,7 +16,20 @@ import org.junit.Test;
  */
 public class UserService {
     @Test
-    public void findById() {
+    public void findById() throws IOException {
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sessionFactory;
 
+        sessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsReader("mybatis-config.xml"));
+
+        SqlSession sqlSession = sessionFactory.openSession();
+
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findById(1L);
+            System.out.println(user);
+        } finally {
+            sqlSession.close();
+        }
     }
 }
