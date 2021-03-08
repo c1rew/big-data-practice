bda56451d118a117a0cb3c6421ab035486c9a66d
c1rew <cholyonzh@gmail.com>
2021-03-08 08:36:45 +0800

    update pom.xml & add user bean

---
+++ Java/learn_springmvc/src/main/java/com/viper/bean/User.java
@@ -0,0 +1,13 @@
+package com.viper.bean;
+
+import lombok.Data;
+
+/**
+ * @author c1rew
+ * @create 2021-03-07 18:43
+ */
+@Data
+public class User {
+    private long id;
+    private String name;
+}
